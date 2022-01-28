56d0fb15c92f4d9e0eb3063f2cffde033bec13b9
calcscout <calcscout@gmail.com>
2022-01-28 07:37:16 +0100

    Done with tool for img generation

--- output/earrings-bayc/generate_imgs.js
+++ output/earrings-bayc/generate_imgs.js
@@ -1,81 +1,83 @@
 const fs = require('fs');
-// const path = require('path');
-// const { createCanvas, loadImage } = require('canvas');
+const path = require('path');
+const { createCanvas, loadImage } = require('canvas');
 const basePath = process.cwd();
-// const buildDir = `./img`;
-// const layersDir = `./layers`;
-// const inputDir = `./layers`;
-// const { format, pixelFormat } = require(`./config`);
-// const console = require('console');
-// const canvas = createCanvas(format.width, format.height);
-// const ctx = canvas.getContext('2d');
-// ctx.imageSmoothingEnabled = format.smoothing;
+const buildDir = `./img`;
+const layersDir = `./layers`;
+const inputDir = `./layers`;
 
-// const buildSetup = () => {
-// 	if (fs.existsSync(buildDir)) {
-// 		fs.rmdirSync(buildDir, { recursive: true });
-// 	}
-// 	fs.mkdirSync(buildDir);
-// };
+const format = {
+	width: 631,
+	height: 631,
+	smoothing: true,
+};
+
+const console = require('console');
+const canvas = createCanvas(format.width, format.height);
+const ctx = canvas.getContext('2d');
+ctx.imageSmoothingEnabled = format.smoothing;
+
+const buildSetup = () => {
+	if (fs.existsSync(buildDir)) {
+		fs.rmdirSync(buildDir, { recursive: true });
+	}
+	fs.mkdirSync(buildDir);
+};
 
 // read json data
 let rawdata = fs.readFileSync(`./metadata/initial/_metadata.json`);
 let data = JSON.parse(rawdata);
 
 const traitsData = data.map((item) => {
-	const attributesImages = {
-		attributes: item.attributes,
-	};
-	return attributesImages;
+	return item.attributes;
 });
 
-console.log(traitsData);
-// const getImages = (_dir) => {
-// 	try {
-// 		return fs
-// 			.readdirSync(_dir)
-// 			.filter((item) => {
-// 				let extension = path.extname(`${_dir}${item}`);
-// 				if (extension == '.png' || extension == '.jpg') {
-// 					return item;
-// 				}
-// 			})
-// 			.map((i) => {
-// 				return {
-// 					filename: i,
-// 					path: `${_dir}/${i}`,
-// 				};
-// 			});
-// 	} catch {
-// 		return null;
-// 	}
-// };
+// console.log(traitsData.length);
+const getImages = (_dir) => {
+	try {
+		return fs
+			.readdirSync(_dir)
+			.filter((item) => {
+				let extension = path.extname(`${_dir}${item}`);
+				if (extension == '.png' || extension == '.jpg') {
+					return item;
+				}
+			})
+			.map((i) => {
+				return {
+					filename: i,
+					path: `${_dir}/${i}`,
+				};
+			});
+	} catch {
+		return null;
+	}
+};
 
-// const images = getImages(inputDir);
+const images = getImages(inputDir);
 // console.log()
 
-// const loadImgData = async (_imgObject) => {
-// 	return new Promise(async (resolve) => {
-// 		const image = await loadImage(`${_imgObject.path}`);
-// 		resolve({ imgObject: _imgObject, loadedImage: image });
-// 	});
-// };
+const loadImgData = async (_imgPath) => {
+	return new Promise(async (resolve) => {
+		const image = await loadImage(`${_imgPath}`);
+		console.log({ _imgPath, loadedImage: image });
+		resolve({ imgPath: _imgPath, loadedImage: image });
+	});
+};
 
-// const draw = (_imgObject) => {
-// 	let size = pixelFormat.ratio;
-// 	let w = canvas.width * size;
-// 	let h = canvas.height * size;
-// 	ctx.imageSmoothingEnabled = false;
-// 	ctx.drawImage(_imgObject.loadedImage, 0, 0, w, h);
-// 	ctx.drawImage(canvas, 0, 0, w, h, 0, 0, canvas.width, canvas.height);
-// };
+const draw = (_imgData) => {
+	// let size = pixelFormat.ratio;
+	let w = canvas.width;
+	let h = canvas.height;
+	ctx.imageSmoothingEnabled = false;
+	console.log('Here: ', _imgData.loadedImage);
+	ctx.drawImage(_imgData.loadedImage, 0, 0, w, h);
+	// ctx.drawImage(canvas, 0, 0, w, h, 0, 0, canvas.width, canvas.height);
+};
 
-// const saveImage = (_loadedImageObject) => {
-// 	fs.writeFileSync(
-// 		`${buildDir}/${_loadedImageObject.imgObject.filename}`,
-// 		canvas.toBuffer('image/png')
-// 	);
-// };
+const saveImage = (_filePath) => {
+	fs.writeFileSync(`${_filePath}`, canvas.toBuffer('image/png'));
+};
 
 // const startCreating = async () => {
 // const images = getImages(inputDir);
@@ -98,5 +100,32 @@ console.log(traitsData);
 // });
 // };
 
-// buildSetup();
-// startCreating();
+console.log(Object.entries(traitsData));
+
+const startCreating = async () => {
+	let imageNum = 0;
+
+	for (let j = 0; j < traitsData.length; j++) {
+		let item = traitsData[j];
+		ctx.clearRect(0, 0, format.width, format.height);
+		console.log(`Canvas cleared`);
+		imageNum++;
+
+		for (let i = 0; i < item.length; i++) {
+			const layer = item[i];
+			const imagePath = `${layersDir}/${layer.trait_type}/${layer.value}.png`;
+			const imgData = await loadImgData(imagePath);
+			console.log('Before drawing', imgData);
+			await draw(imgData);
+			console.log('After drawing');
+		}
+		console.log(`Image ${imageNum} created!`);
+		const filePath = `${buildDir}/${imageNum}.png`;
+		console.log(`File path ${filePath}`);
+		await saveImage(filePath);
+		console.log(`File saved`);
+	}
+};
+
+buildSetup();
+startCreating();
--- src/config.js
+++ src/config.js
@@ -1,144 +1,30 @@
-const basePath = process.cwd();
-const { MODE } = require(`${basePath}/constants/blend_mode.js`);
-const { NETWORK } = require(`${basePath}/constants/network.js`);
+//here you define which config to use
+const configToUse = './config_earrings_bayc';
 
-const network = NETWORK.eth;
-
-//AM defined modifications
-const collectionName = 'Bongs';
-const collectionFolder = 'bongs';
-const containRaces = false; //if false, only one race is considered
-const suitingMouthNumbers = [1, 2]; //ignored if there are races
-
-const mouthNumbers = {
-	bored: 1,
-	boredUnshaven: 2,
-	grin: 3,
-	boredCigarette: 4,
-	dumbfounded: 5,
-	boredUnshavenCigarette: 6,
-	phonemeVuh: 7,
-	jovial: 8,
-	smallGrin: 9,
-	rage: 10,
-	phonemeOoo: 11,
-	phonemeL: 12,
-	phonemeOh: 13,
-	discomfort: 14,
-	tongueOut: 15,
-	phonemeWah: 16,
-	boredPipe: 17,
-	boredCigar: 18,
-	boredBubblegum: 19,
-	grinMulticolored: 20,
-	boredUnshavenPipe: 21,
-	boredUnshavenCigar: 22,
-	grinGoldGrill: 23,
-	boredPartyHorn: 24,
-	grinDiamondGrill: 25,
-	boredKazoo: 26,
-	boredUnshavenBubblegum: 27,
-	boredUnshavenKazoo: 28,
-	boredPizza: 29,
-	boredDagger: 30,
-	boredUnshavenPartyHorn: 31,
-	boredUnshavenDagger: 32,
-	boredUnshavenPizza: 33,
-};
-
-// General metadata for Ethereum
-const namePrefix = 'Bong';
-const description = 'Collection of NFT bongs for your Apes';
-const baseUri = 'ipfs://QmZVzDfSWLpccLCMLwqzaASo15PPEmZs6GNTWKMm8Z2Dpt';
-
-const solanaMetadata = {
-	symbol: 'YC',
-	seller_fee_basis_points: 100, // Define how much % you want from secondary market sales 1000 = 10%
-	external_url: 'https://www.youtube.com/c/hashlipsnft',
-	creators: [
-		{
-			address: '7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC',
-			share: 100,
-		},
-	],
-};
-
-// If you have selected Solana then the collection starts from 0 automatically
-const layerConfigurations = [
-	{
-		growEditionSizeTo: 1100,
-		layersOrder: [
-			{ name: 'Mouth' },
-			{ name: 'Bong' },
-			{ name: 'Fill' },
-			{ name: 'Smoke' },
-			{ name: 'Sticker' },
-		],
-	},
-];
-
-const shuffleLayerConfigurations = true;
-
-const debugLogs = false;
-
-const format = {
-	width: 631,
-	height: 631,
-	smoothing: true,
-};
-
-const gif = {
-	export: false,
-	repeat: 0,
-	quality: 100,
-	delay: 500,
-};
-
-const text = {
-	only: false,
-	color: '#ffffff',
-	size: 20,
-	xGap: 40,
-	yGap: 40,
-	align: 'left',
-	baseline: 'top',
-	weight: 'regular',
-	family: 'Courier',
-	spacer: ' => ',
-};
-
-const pixelFormat = {
-	ratio: 2 / 128,
-};
-
-const background = {
-	generate: false,
-	brightness: '80%',
-	static: false,
-	default: '#000000',
-};
-
-const extraMetadata = {};
-
-const rarityDelimiter = '#';
-
-const uniqueDnaTorrance = 10000;
-
-const preview = {
-	thumbPerRow: 5,
-	thumbWidth: 50,
-	imageRatio: format.height / format.width,
-	imageName: 'preview.png',
-};
-
-const preview_gif = {
-	numberOfImages: 5,
-	order: 'ASC', // ASC, DESC, MIXED
-	repeat: 0,
-	quality: 100,
-	delay: 500,
-	imageName: 'preview.gif',
-};
+const {
+	format,
+	baseUri,
+	description,
+	background,
+	uniqueDnaTorrance,
+	layerConfigurations,
+	rarityDelimiter,
+	preview,
+	shuffleLayerConfigurations,
+	debugLogs,
+	extraMetadata,
+	pixelFormat,
+	text,
+	namePrefix,
+	network,
+	solanaMetadata,
+	gif,
+	preview_gif,
+	collectionName,
+	collectionFolder,
+	containRaces,
+	suitingMouthNumbers,
+} = require(`${configToUse}`);
 
 module.exports = {
 	format,
